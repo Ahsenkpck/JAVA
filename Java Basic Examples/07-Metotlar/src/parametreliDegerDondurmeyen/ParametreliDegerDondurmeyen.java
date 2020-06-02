7b8f680880893ebfacc95a414581546da922bec6
Ahsen Kıpçak <akipcak01@gmail.com>
2020-06-02 18:22:14 +0300

    Add files via upload

---
+++ Java Basic Examples/07-Metotlar/src/parametreliDegerDondurmeyen/ParametreliDegerDondurmeyen.java
@@ -0,0 +1,29 @@
+package parametreliDegerDondurmeyen;
+
+/**
+ *
+ * @author akipc
+ */
+public class ParametreliDegerDöndermeyen {
+
+    public static void topla(int a, int b, int c, double d) {
+        int toplam = a + b + c;
+        double kök = Math.sqrt(d);
+        System.out.println("Toplam sonucu:" + toplam);
+        System.out.println("Kök değeri:" + kök);
+    }
+
+    public static void carp(int a, int b) {
+        int carpım = a * b;
+        System.out.println("Çarpım sonucu:" + carpım);
+
+    }
+
+    public static void main(String[] args) {
+        topla(6, 11, 20, 2.7);
+        topla(5, 3, 30, 5.9);
+        carp(3, 9);
+        carp(12, 18);
+    }
+
+}
---
+++ Java Basic Examples/07-Metotlar/src/parametresizDegerDondurmeyen/ParametresizDegerDondurmeyen.java
@@ -0,0 +1,25 @@
+package parametresizDegerDondurmeyen;
+
+/**
+ *
+ * @author akipc
+ */
+public class ParametresizDegerDöndermeyen {
+
+    public static void main(String[] args) {
+
+        yazdır();
+        System.out.println("");
+        yazdır();
+    }
+
+    /*
+     *void-<deger döndermeyen metod.Hrhangi bir sonuç üretmez
+     */
+    public static void yazdır() {
+        System.out.println("Merhaba");
+        System.out.println("Ahsen");
+        System.out.println("Nasılsın");
+        System.out.println("İyi misin");
+    }
+}
